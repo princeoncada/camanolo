581cf81be83cf67e4541529908abc637ae7504f1
princeoncada <prince.oncada@gmail.com>
2026-04-30 13:17:17 -0400

    feat(separator): add Separator component for visual separation in layouts

---
+++ components/sections/Separator.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Separator = () => {
+  return (
+    <div className="w-full h-px bg-linear-to-r from-transparent via-black/20 to-transparent"></div>
+  );
+};
+
+export default Separator;
